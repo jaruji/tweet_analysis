707d40dbf0927e94fa17b36d889da692e6b177e5
Juraj Bedej <xbedej@stuba.sk>
2021-10-08 15:39:42 +0200

    working on task 4

--- src/tweetAnalyser.py
+++ src/tweetAnalyser.py
@@ -73,11 +73,41 @@ def addTweetConspiracyTable(conn, cur):
     )
     conn.commit()
 
+def addConspiracyInWeeksTable(conn, cur):
+    cur.execute("""CREATE TABLE conspiracies_in_weeks (
+        id SERIAL PRIMARY KEY,
+        week float,
+        year float,
+        conspiracy_id int NOT NULL,
+        tweet_ratio float,
+        tweet_count bigint,
+        tweet_extreme_count bigint,
+        tweet_neutral_count bigint,
+        FOREIGN KEY(conspiracy_id) REFERENCES conspiracies(id)
+        )"""
+    )
+    conn.commit()
+
+def fillConspiracyInWeeksTable(cur):
+    cur.execute("""INSERT INTO conspiracies_in_weeks(week, year, conspiracy_id, tweet_ratio, tweet_count, tweet_extreme_count, tweet_neutral_count)
+        SELECT date_week, date_year, conspiracy_id, CASE WHEN tweet_neutral_count = 0 THEN NULL ELSE CAST(tweet_extreme_count as float) / CAST(tweet_neutral_count as float) END AS ratio, tweet_count, tweet_extreme_count, tweet_neutral_count
+        FROM (
+        SELECT date_part('week', t.happened_at) date_week, date_part('year', t.happened_at) date_year, tc.conspiracy_id,
+        count(*) tweet_count, sum(CASE WHEN t.compound >= 0.5 OR t.compound <= -0.5 THEN 1 ELSE 0 END) tweet_extreme_count,
+        sum(CASE WHEN t.compound < 0.5 AND t.compound > -0.5 THEN 1 ELSE 0 END) tweet_neutral_count
+        FROM tweet_conspiracies as tc 
+        JOIN tweets as t on t.id = tc.tweet_id 
+        GROUP BY date_part('week', t.happened_at), date_part('year', t.happened_at), tc.conspiracy_id
+        ORDER BY date_part('year', t.happened_at) asc, date_part('week', t.happened_at) asc) as sub;
+        """
+    )
+
 
 def insertSentiment(tweetId, dict, cur):
     cur.execute("UPDATE tweets SET neu = %s, neg = %s, pos = %s, compound = %s WHERE tweets.id = %s;", (dict['neu'], dict['neg'], dict['pos'], dict['compound'], tweetId))
 
 def insertTweetConspiracy(cur):
+    #create many-to-many table between tables tweets and conspiracies (mapping only conspiracy tweets)
     cur.execute("""INSERT INTO tweet_conspiracies(tweet_id, conspiracy_id)
         SELECT DISTINCT sub.id, sub.conspiracy FROM (SELECT t.id, CASE 
         WHEN hash.value ILIKE '%deepstate%' THEN (SELECT id FROM conspiracies WHERE value ILIKE  '%Deepstate%')
@@ -99,41 +129,46 @@ def insertTweetConspiracy(cur):
         WHERE sub.conspiracy IS NOT NULL;"""
     )
 
-def executeSelect(conn):
-    start = time.time();
-    df = sqlio.read_sql_query("""SELECT DISTINCT(t.id), t.content FROM tweets as t
-                              JOIN tweet_hashtags as th ON t.id=th.tweet_id
-                              JOIN hashtags as h ON th.hashtag_id=h.id
-                              WHERE h.value ILIKE any(array[
-                                  '%DeepstateVirus%',
-                                  '%DeepStateFauci%',
-                                  '%DeepStateVaccine%',
-                                  '%QAnon%',
-                                  '%Agenda21%',
-                                  '%CCPVirus%',
-                                  '%ClimateChangeHoax%',
-                                  '%GlobalWarmingHoax%',
-                                  '%ChinaLiedPeopleDied%',
-                                  '%5GCoronavirus%',
-                                  '%SorosVirus%',
-                                  '%MAGA%',
-                                  '%WWG1WGA%',
-                                  '%Chemtrails%',
-                                  '%flatEarth%',
-                                  '%MoonLandingHoax%',
-                                  '%moonhoax%',
-                                  '%911truth%',
-                                  '%911insidejob%',
-                                  '%illuminati%',
-                                  '%reptilians%',
-                                  '%pizzaGateIsReal%',
-                                  '%PedoGateIsReal%'
-                              ])""", conn)
-    print("Number of rows: ", df.shape[0])
-    end = time.time();
+def generateGraphs(cur):
+    cur.execute("SELECT week, year")
+
+def execute(conn):
+    # start = time.time();
+    # df = sqlio.read_sql_query("""SELECT DISTINCT(t.id), t.content FROM tweets as t
+    #                           JOIN tweet_hashtags as th ON t.id=th.tweet_id
+    #                           JOIN hashtags as h ON th.hashtag_id=h.id
+    #                           WHERE h.value ILIKE any(array[
+    #                               '%DeepstateVirus%',
+    #                               '%DeepStateFauci%',
+    #                               '%DeepStateVaccine%',
+    #                               '%QAnon%',
+    #                               '%Agenda21%',
+    #                               '%CCPVirus%',
+    #                               '%ClimateChangeHoax%',
+    #                               '%GlobalWarmingHoax%',
+    #                               '%ChinaLiedPeopleDied%',
+    #                               '%5GCoronavirus%',
+    #                               '%SorosVirus%',
+    #                               '%MAGA%',
+    #                               '%WWG1WGA%',
+    #                               '%Chemtrails%',
+    #                               '%flatEarth%',
+    #                               '%MoonLandingHoax%',
+    #                               '%moonhoax%',
+    #                               '%911truth%',
+    #                               '%911insidejob%',
+    #                               '%illuminati%',
+    #                               '%reptilians%',
+    #                               '%pizzaGateIsReal%',
+    #                               '%PedoGateIsReal%'
+    #                           ])""", conn)
+    # print("Number of rows: ", df.shape[0])
+    # end = time.time();
     # print("Select completed, elapsed time was ", end - start, "\n");
-    start = time.time()
+    # start = time.time()
     cur = conn.cursor()
+
+
     #uloha 2
     #############################################
     #runtime 1hour
@@ -142,6 +177,8 @@ def executeSelect(conn):
     #df.apply(lambda x: handleTweet(x['id'], x['content'], cur, sia), axis = 1)
     #DONE
     #############################################
+
+
     #uloha3
     #############################################
     # try:
@@ -155,21 +192,30 @@ def executeSelect(conn):
     #     , 'COVID19 and microchipping', 'COVID19 is spreaded by 5G', 'Moon landing is fake', '9/11 was an inside job'
     #     , 'Pizzagate conspiracy theory', 'Chemtrails' , 'FlatEarth', 'Illuminati', 'Reptilian conspiracy theory']
     # )
-    start = time.time()
-    insertTweetConspiracy(cur)
-    end = time.time();
-    print("Insert many-to-many completed, elapsed time was ", end - start, "\n");
-    ##create table to map hashtags to theories
-    #then INSERT a SELECT into tweet_conspiracies table...
-
-
+    #start = time.time()
+    #insertTweetConspiracy(cur)
+    #end = time.time();
+    #print("Insert many-to-many completed, elapsed time was ", end - start, "\n");
+    #DONE
     #############################################
 
 
 
     #uloha4
     #############################################
-    #BLABLABLA
+    start = time.time()
+    
+    try:
+        addConspiracyInWeeksTable(conn, cur)
+        print("Conspiracy in weeks table created")
+        fillConspiracyInWeeksTable(cur)
+        conn.commit()
+        end = time.time();
+        print("Conspiracy in weeks table filled, elapsed time was ", end - start, "\n");
+    except:
+        print("Drop the table and try again.")
+    
+    #generateGraphs(cur)
     #############################################
     #uloha5
     #############################################
@@ -181,8 +227,8 @@ def executeSelect(conn):
     #############################################
     conn.commit()
     cur.close()
-    end = time.time()
-    print("Update completed, elapsed time was ", end - start, "\n")
+    #end = time.time()
+    #print("Update completed, elapsed time was ", end - start, "\n")
 
 #main
 if __name__ == "__main__":
@@ -190,7 +236,7 @@ if __name__ == "__main__":
     #main pipeline
     #select tweets with conspiracy hashtags, remove emotes, mentions and hashtags.
     #calculate sentiment using VADER library
-    executeSelect(conn)
+    execute(conn)
     if conn is not None:
         conn.close()
         print("Connection closed.")
